7472a10479a013dcc337db41835ed353f8247921
21120172 <nguyentuandat2k3cmg@gmail.com>
2023-12-15 14:05:15 +0700

    upload login

---
+++ MyShop/Services/RelativeToAbsoluteConverter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Services
+{
+    class RelativeToAbsoluteConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var imageFile = (string)value;
+            var currentFolder = AppDomain.CurrentDomain.BaseDirectory;
+            return currentFolder + imageFile;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
