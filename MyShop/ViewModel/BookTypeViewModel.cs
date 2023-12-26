e38f0ec978262e6976b5b9ec5279cb8036126f89
21120172 <nguyentuandat2k3cmg@gmail.com>
2023-12-26 10:23:46 +0700

    Fix book types (book genres)

--- MyShop/ViewModel/BookTypeViewModel.cs
+++ MyShop/ViewModel/BookTypeViewModel.cs
@@ -12,20 +12,36 @@ using System.Windows.Input;
 using Windows.Storage.Pickers;
 using Windows.Storage;
 using System.Windows.Forms;
+using System.ComponentModel;
+using System.Threading;
+using Windows.UI.WebUI;
 
 namespace MyShop.ViewModel
 {
     public class BookTypeViewModel : ViewModelBase
     {
         private List<Genre> _genres;
-        private ObservableCollection<Genre> _displayGenresCollection;
+        private ObservableCollection<GenreRow> _displayGenreRowsCollection;
         private IBookRepository _bookRepository;
-        private Genre _selectedGenre;
+        private GenreRow _selectedGenreRow;
+
+        private RelayCommand _addCommand;
+        private RelayCommand _deleteCommand;
+        private RelayCommand _saveCommand;
+        private RelayCommand<object> _gotFocusCommand;
+        public List<Genre> Genres { get => _genres; set => _genres = value; }
+        public ObservableCollection<GenreRow> DisplayGenreRowsCollection { get => _displayGenreRowsCollection; set => _displayGenreRowsCollection = value; }
+        public IBookRepository BookRepository { get => _bookRepository; set => _bookRepository = value; }
+        public GenreRow SelectedGenreRow { get => _selectedGenreRow; set => _selectedGenreRow = value; }
+        public RelayCommand AddCommand { get => _addCommand; set => _addCommand = value; }
+        public RelayCommand DeleteCommand { get => _deleteCommand; set => _deleteCommand = value; }
+        public RelayCommand SaveCommand { get => _saveCommand; set => _saveCommand = value; }
+        public RelayCommand<object> GotFocusCommand { get => _gotFocusCommand; set => _gotFocusCommand = value; }
 
         public BookTypeViewModel()
         {
             _bookRepository = new BookRepository();
-            DisplayGenresCollection = new ObservableCollection<Genre>();
+            DisplayGenreRowsCollection = new ObservableCollection<GenreRow>();
             PageLoaded();
 
             AddCommand = new RelayCommand(ExecuteAddCommand);
@@ -36,31 +52,29 @@ namespace MyShop.ViewModel
         private void ExecuteGotFocusCommand(object sender)
         {
             TextBox textBox = sender as TextBox;
-
         }
 
         private async void ExecuteSaveCommand()
         {
-            if (SelectedGenre == null)
+            if (SelectedGenreRow == null)
             {
                 await App.MainRoot.ShowDialog("No selected item", "Please select an item first!");
                 return;
             }
 
-            var task = await _bookRepository.EditGenre(SelectedGenre);
+            var task = await _bookRepository.EditGenre(new Genre() { Id = SelectedGenreRow.Id, Name = SelectedGenreRow.Name});
             UpdateDataSource();
         }
 
         private async void ExecuteDeleteCommand()
         {
-            if (SelectedGenre == null)
+            if (SelectedGenreRow == null)
             {
                 await App.MainRoot.ShowDialog("No selected item", "Please select an item first!");
                 return;
             }
 
-            var task = await _bookRepository.RemoveGenre(SelectedGenre.Id);
-
+            var task = await _bookRepository.RemoveGenre(SelectedGenreRow.Id);
             UpdateDataSource();
         }
 
@@ -68,7 +82,6 @@ namespace MyShop.ViewModel
         {
             var task = await _bookRepository.AddGenre(new Genre { Name = "New Genre" }) ;
             UpdateDataSource();
-
         }
         public void PageLoaded()
         {
@@ -78,23 +91,26 @@ namespace MyShop.ViewModel
         public async void UpdateDataSource()
         {
             Genres = await _bookRepository.GetGenres();
-            DisplayGenresCollection.Clear();
-            Genres.ForEach(genre => DisplayGenresCollection.Add(genre));
+            DisplayGenreRowsCollection.Clear();
+
+            for (int i = 0; i < Genres.Count; i++)
+            {
+                DisplayGenreRowsCollection.Add(new GenreRow()
+                {
+                    No = i + 1,
+                    Id = Genres[i].Id,
+                    Name = Genres[i].Name,
+                });
+            }
         }
 
-        private RelayCommand _addCommand;
-        private RelayCommand _deleteCommand;
-        private RelayCommand _saveCommand;
-        private RelayCommand<object> _gotFocusCommand;
+        public class GenreRow: INotifyPropertyChanged
+        {
+            public int No { get; set; }
+            public int Id { get; set; }
+            public string Name { get; set; }
 
-
-        public List<Genre> Genres { get => _genres; set => _genres = value; }
-        public ObservableCollection<Genre> DisplayGenresCollection { get => _displayGenresCollection; set => _displayGenresCollection = value; }
-        public IBookRepository BookRepository { get => _bookRepository; set => _bookRepository = value; }
-        public Genre SelectedGenre { get => _selectedGenre; set => _selectedGenre = value; }
-        public RelayCommand AddCommand { get => _addCommand; set => _addCommand = value; }
-        public RelayCommand DeleteCommand { get => _deleteCommand; set => _deleteCommand = value; }
-        public RelayCommand SaveCommand { get => _saveCommand; set => _saveCommand = value; }
-        public RelayCommand<object> GotFocusCommand { get => _gotFocusCommand; set => _gotFocusCommand = value; }
+            public event PropertyChangedEventHandler PropertyChanged;
+        }
     }
 }
